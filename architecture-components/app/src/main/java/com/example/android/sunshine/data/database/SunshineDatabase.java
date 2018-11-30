5e787efff837a4404bba0c1ba6cf7756356a0b16
laramartin <laramartin@users.noreply.github.com>
2018-11-30 17:55:02 +0100

    Adds typeconvertere for Date class

--- architecture-components/app/src/main/java/com/example/android/sunshine/data/database/SunshineDatabase.java
+++ architecture-components/app/src/main/java/com/example/android/sunshine/data/database/SunshineDatabase.java
@@ -3,9 +3,11 @@ package com.example.android.sunshine.data.database;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
 import android.content.Context;
 
 @Database(entities = {WeatherEntry.class}, version = 1)
+@TypeConverters(DateConverter.class)
 public abstract class SunshineDatabase extends RoomDatabase {
 
     public abstract WeatherDao weatherDao();
