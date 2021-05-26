176691e0dc3a55729cdfe243762584a33e3f6aa9
Lara Martin <13904500+laramartin@users.noreply.github.com>
2021-05-26 18:27:44 +0200

    flexible layouts

--- compose-codelab-basics/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ compose-codelab-basics/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -1,36 +1,86 @@
 package com.example.basicscodelab
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      BasicsCodelabTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(color = MaterialTheme.colors.background) {
-          Greeting("Android")
-        }
-      }
+//      MyApp { Greeting("Hello Android!") }
+      MyApp { MyScreenContent() }
     }
   }
 }
 
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+  BasicsCodelabTheme {
+    // A surface container using the 'background' color from the theme
+    Surface(color = MaterialTheme.colors.background) {
+      content()
+    }
+  }
 }
 
-@Preview(showBackground = true, name = "Text preview")
+@Composable
+private fun Greeting(name: String) {
+  Surface(color = Color.Cyan) {
+    Text(text = "Hello $name!", modifier = Modifier.padding(32.dp))
+  }
+}
+
+@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
   BasicsCodelabTheme {
-    Greeting(name = "Android")
+    Greeting("Android")
+  }
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+
+  val counterState = remember { mutableStateOf(0) }
+
+  Column(modifier = Modifier.fillMaxHeight()) {
+    NameList(names, modifier = Modifier.weight(1f))
+    Divider(color = Color.Transparent, thickness = 32.dp)
+    Counter(
+      count = counterState.value,
+      updateCount = { newCount ->
+        counterState.value = newCount
+      }
+    )
+  }
+}
+
+@Composable
+private fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+  Column(modifier = modifier) {
+    for (name in names) {
+      Greeting(name = name)
+      Divider(color = Color.Black)
+    }
+  }
+}
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+  Button(onClick = { updateCount(count + 1) }) {
+    Text("I've been clicked $count times")
   }
 }
